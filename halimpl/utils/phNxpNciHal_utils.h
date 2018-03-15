3d55a453800664ea66eb0705afa0a025f91071e6
nxf24591 <nanjesh.s_1@nxp.com>
2018-03-15 12:35:38 +0530

    [artf562056] : Copyright year update

--- halimpl/utils/phNxpNciHal_utils.h
+++ halimpl/utils/phNxpNciHal_utils.h
@@ -1,6 +1,6 @@
 /*
  *
- *  Copyright (C) 2013-2014 NXP Semiconductors
+ *  Copyright (C) 2013-2018 NXP Semiconductors
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
