b6d83c5bbf0ae5bb39e7ce8c287835349e8b75e3
nxf35421 <sangeetha.agarwal@nxp.com>
2018-05-18 11:17:53 +0530

    MW version updated to 9.0.4

--- halimpl/hal/phNxpNciHal.cc
+++ halimpl/hal/phNxpNciHal.cc
@@ -64,7 +64,7 @@ do\
     goto recoveryLabel;\
 }while(0)
 
-static const char* NXP_NFC_MW_VER = "NFC_NCIHALx_AR0040.9.0.3";
+static const char* NXP_NFC_MW_VER = "NFC_NCIHALx_AR0040.9.0.4";
 /* Processing of ISO 15693 EOF */
 extern uint8_t icode_send_eof;
 extern uint8_t icode_detected;
