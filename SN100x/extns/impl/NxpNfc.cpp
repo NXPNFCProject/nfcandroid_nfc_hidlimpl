9bfb79588acdc8a4d88db252354d5ce07c6cc55c
nxf24178 <ravindra.yashvant.shinde@nxp.com>
2018-10-31 11:11:10 +0530

    China Transit: libnfc-nxpTrnasit.conf should be null when Default config is set from the NXPTransitWallet.apk
    Reverted all the changes done in the gerVendorConfig() method as it is google AOSP and must not be changed.
    Also reverting the change done for the China transit in the previous commit, as after taking a patch in libnfc-nci it is not needed.
    In case of the default config setting, HAL will delete the libnfc-nxpTransit.conf file.

--- SN100x/extns/impl/NxpNfc.cpp
+++ SN100x/extns/impl/NxpNfc.cpp
@@ -46,8 +46,12 @@ Return<void> NxpNfc::ioctl(uint64_t ioctlType, const hidl_vec<uint8_t>& inOutDat
   if (ioctlType == HAL_NFC_IOCTL_SET_TRANSIT_CONFIG) {
     /*As transit configurations are appended at the end of
     nfc_nci_IoctlInOutData_t, Assign appropriate pointer to TransitConfig*/
-    inpOutData.inp.data.transitConfig.val =
-        ((char *)pInOutData) + sizeof(nfc_nci_IoctlInOutData_t);
+    if (inpOutData.inp.data.transitConfig.len == 0) {
+      inpOutData.inp.data.transitConfig.val = NULL;
+    } else {
+      inpOutData.inp.data.transitConfig.val =
+          ((char *)pInOutData) + sizeof(nfc_nci_IoctlInOutData_t);
+      }
   }
     status = phNxpNciHal_ioctl(ioctlType, &inpOutData);
     if(HAL_NFC_IOCTL_ESE_JCOP_DWNLD == ioctlType)
