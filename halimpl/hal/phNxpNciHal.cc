80ae929d3e70a492d4995dbd0d065bb08e2b070a
nxf35421 <sangeetha.agarwal@nxp.com>
2019-04-25 18:25:31 +0530

    First push on Android Q RoW branch

--- halimpl/hal/phNxpNciHal.cc
+++ halimpl/hal/phNxpNciHal.cc
@@ -2459,9 +2459,6 @@ int phNxpNciHal_core_initialized(uint8_t* p_core_init_rsp_params) {
                                                nxpncihal_ctrl.p_rx_data);
     }
   }
-  if (isNxpConfigModified()) {
-    updateNxpConfigTimestamp();
-  }
   if (config_success == false)
     return NFCSTATUS_FAILED;
   else
