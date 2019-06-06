8daca8991e22205dbad1efc5625685def4291d11
nxf48293 <priyanka.rajput@nxp.com>
2019-06-06 14:59:44 +0530

    [artf659667] [COVERITY]: Fixed coverity warnings.
    Updated License year.

--- SN100x/halimpl/src/adaptation/EseAdaptation.cc
+++ SN100x/halimpl/src/adaptation/EseAdaptation.cc
@@ -1,7 +1,7 @@
 /******************************************************************************
  *
  *
- *  Copyright (C) 2015 NXP Semiconductors
+ *  Copyright (C) 2015-2019 NXP Semiconductors
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
