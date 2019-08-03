6ede04b352c82ced9430ed4bc1d3fd1adb05f41c
nxf24178 <ravindra.yashvant.shinde@nxp.com>
2019-08-03 18:44:03 +0530

    Redesign to make common code base
    During boot time, If NFCC is not in NFC Forum mode then it shall
    be set back to it.

--- SN100x/halimpl/utils/phNxpConfig.h
+++ SN100x/halimpl/utils/phNxpConfig.h
@@ -173,6 +173,8 @@ extern char Fw_Lib_Path[256];
 #define NAME_FW_STORAGE "FW_STORAGE"
 #define NAME_NXP_CORE_CONF "NXP_CORE_CONF"
 #define NAME_NXP_GUARD_TIMER_VALUE "NXP_GUARD_TIMER_VALUE"
+#define NAME_NXP_PROP_RESET_EMVCO_CMD "NXP_PROP_RESET_EMVCO_CMD"
+#define NAME_NFA_CONFIG_FORMAT "NFA_CONFIG_FORMAT"
 #endif
 /* default configuration */
 #define default_storage_location "/data/vendor/nfc"
