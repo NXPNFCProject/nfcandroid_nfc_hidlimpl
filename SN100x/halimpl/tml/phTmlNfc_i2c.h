757657678222646488859a57636089d471c8e213
Pratap Reddy Vaka <vakapratapa.reddy@nxp.com>
2019-08-30 12:02:21 +0530

    [artf681821][FWDWNLD]: Fix incorrect header length read during I2C read
    MW updating the FW download header length for read just after making the FW GPIO high.
    Due to which there is a chnace of race condition when NCI ntf are received at the same time.
    To avoid such scenario, header length updated just before issuing the FW dwnld start seq.

--- SN100x/halimpl/tml/phTmlNfc_i2c.h
+++ SN100x/halimpl/tml/phTmlNfc_i2c.h
@@ -41,6 +41,7 @@ int phTmlNfc_i2c_read(void* pDevHandle, uint8_t* pBuffer, int nNbBytesToRead);
 int phTmlNfc_i2c_write(void* pDevHandle, uint8_t* pBuffer, int nNbBytesToWrite);
 int phTmlNfc_i2c_reset(void* pDevHandle, long level);
 bool_t getDownloadFlag(void);
+void phTmlNfc_EnableFwDnldMode(bool mode);
 extern bool_t notifyFwrequest;
 extern phTmlNfc_i2cfragmentation_t fragmentation_enabled;
 
