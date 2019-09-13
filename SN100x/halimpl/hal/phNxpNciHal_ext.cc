02cdff018e759f6338f440adf096a6257a564e69
nxf24178 <ravindra.yashvant.shinde@nxp.com>
2019-09-13 17:17:47 +0530

    [artf682848][AOSP]: Prevent OOB read in phNxpNciHal_ext_process_nfc_init_rsp

--- SN100x/halimpl/hal/phNxpNciHal_ext.cc
+++ SN100x/halimpl/hal/phNxpNciHal_ext.cc
@@ -446,6 +446,11 @@ static NFCSTATUS phNxpNciHal_ext_process_nfc_init_rsp(uint8_t* p_ntf, uint16_t* 
           if(!nxpncihal_ctrl.hal_open_status)
             phNxpNciHal_configFeatureList(p_ntf,*p_len);
           int len = p_ntf[2] + 2; /*include 2 byte header*/
+          if (len != *p_len - 1) {
+            NXPLOG_NCIHAL_E("phNxpNciHal_ext_process_nfc_init_rsp invalid NTF length");
+            android_errorWriteLog(0x534e4554, "121263487");
+            return NFCSTATUS_FAILED;
+          }
           wFwVerRsp = (((uint32_t)p_ntf[len - 2])<< 16U)|
           (((uint32_t)p_ntf[len - 1])<< 8U)|p_ntf[len];
           fw_maj_ver = p_ntf[len - 1];
@@ -474,6 +479,11 @@ static NFCSTATUS phNxpNciHal_ext_process_nfc_init_rsp(uint8_t* p_ntf, uint16_t* 
            phNxpNciHal_configFeatureList(p_ntf,*p_len);
         }
         int len = p_ntf[2] + 2; /*include 2 byte header*/
+        if (len != *p_len - 1) {
+          NXPLOG_NCIHAL_E("phNxpNciHal_ext_process_nfc_init_rsp invalid NTF length");
+          android_errorWriteLog(0x534e4554, "121263487");
+          return NFCSTATUS_FAILED;
+        }
         wFwVerRsp = (((uint32_t)p_ntf[len - 2]) << 16U) |
                     (((uint32_t)p_ntf[len - 1]) << 8U) | p_ntf[len];
         if (wFwVerRsp == 0)
