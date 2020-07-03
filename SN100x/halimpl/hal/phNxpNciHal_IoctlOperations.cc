bc05669ff180873df87fa7da57b5fae685d3207d
nxp74772 <suhas.suresh_1@nxp.com>
2020-07-03 15:02:05 +0530

    Add config to select WLC Modes
    A new config parameter NXP_WLC_MODE is introduced.
    WLC mode can be either autonomous(0x01) or
    non-autonomous(0x00).

--- SN100x/halimpl/hal/phNxpNciHal_IoctlOperations.cc
+++ SN100x/halimpl/hal/phNxpNciHal_IoctlOperations.cc
@@ -193,7 +193,8 @@ std::set<string> gNciConfigs = {"NXP_SE_COLD_TEMP_ERROR_DELAY",
                                 "NXP_ENABLE_DISABLE_LOGS",
                                 "NXP_RDR_DISABLE_ENABLE_LPCD",
                                 "NXP_SUPPORT_NON_STD_CARD",
-                                "NXP_GET_HW_INFO_LOG"};
+                                "NXP_GET_HW_INFO_LOG",
+                                "NXP_WLC_MODE"};
 
 /****************************************************************
  * Local Functions
