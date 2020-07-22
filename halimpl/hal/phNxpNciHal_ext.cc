ba44762ed7d71131141e79450f2318d9baaff78c
Ganesh Deva <ganesh.deva_1@nxp.com>
2020-07-22 20:06:07 +0530

    Prevent OOB write in phNxpNciHal_write_ext

--- halimpl/hal/phNxpNciHal_ext.cc
+++ halimpl/hal/phNxpNciHal_ext.cc
@@ -837,7 +837,8 @@ NFCSTATUS phNxpNciHal_write_ext(uint16_t* cmd_len, uint8_t* p_cmd_data,
     }
   }
 
-  if (bEnableMfcReader && p_cmd_data[0] == 0x21 && p_cmd_data[1] == 0x00 &&
+  if (bEnableMfcReader && *cmd_len <= (NCI_MAX_DATA_LEN - 3) &&
+      p_cmd_data[0] == 0x21 && p_cmd_data[1] == 0x00 &&
       p_cmd_data[2] != 0x04 && p_cmd_data[6] != 0x83) {
     NXPLOG_NCIHAL_D("Going through extns - Adding Mifare in RF Discovery");
     p_cmd_data[2] += 3;
