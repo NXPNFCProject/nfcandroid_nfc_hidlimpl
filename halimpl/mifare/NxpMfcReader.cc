c1b4e9417c50b434d1c92807896e282094c74bb4
nxf56319 <sai.shwethas@nxp.com>
2020-08-17 18:25:41 +0530

    Fix for Coverity error_type OVERLAPPING_COPY
    As memcpy commmand is replaced by memmove command as memcpy leads
    to overlapped addresses and results in loss of data.

--- halimpl/mifare/NxpMfcReader.cc
+++ halimpl/mifare/NxpMfcReader.cc
@@ -130,7 +130,7 @@ void NxpMfcReader::BuildAuthCmd() {
   mMfcTagCmdIntfData.sendBufLen = 0x03;
   if (!isPreloadedKey) {
     byKey |= MFC_EMBEDDED_KEY;
-    memcpy(&mMfcTagCmdIntfData.sendBuf[3], &mMfcTagCmdIntfData.sendBuf[6],
+    memmove(&mMfcTagCmdIntfData.sendBuf[3], &mMfcTagCmdIntfData.sendBuf[6],
            MFC_AUTHKEYLEN);
     mMfcTagCmdIntfData.sendBufLen += MFC_AUTHKEYLEN;
   }
