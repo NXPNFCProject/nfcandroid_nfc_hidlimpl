1093318b61da1f25c406c84b6aa55b5c6639f270
hariprasad nalacheruvu <hariprasad.nalacheruvu@nxp.com>
2021-02-17 15:18:18 +0530

    [artf820757][FW]: Add compile time FW library path selection
    To enable compile time FW library path selection based on
    target architecture 32/64-bit

--- SN100x/halimpl/common/phNfcCommon.h
+++ SN100x/halimpl/common/phNfcCommon.h
@@ -1,19 +1,20 @@
-/*
- * Copyright (C) 2010-2018 NXP Semiconductors
+/******************************************************************************
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  Copyright 2010-2018, 2021 NXP
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ ******************************************************************************/
 /*
  *  OSAL header files related to memory, debug, random, semaphore and mutex
  * functions.
@@ -35,10 +36,6 @@
 
 #define FW_DLL_ROOT_DIR "/system/vendor/"
 #define FW_DLL_EXTENSION ".so"
-#define FW_LIB_ROOT_DIR "/vendor/lib64/"
-#define FW_BIN_ROOT_DIR "/vendor/firmware/"
-#define FW_LIB_EXTENSION ".so"
-#define FW_BIN_EXTENSION ".bin"
 
 /* Actual FW library name*/
 /* Restore Corrupted PLL Settings/etc */
