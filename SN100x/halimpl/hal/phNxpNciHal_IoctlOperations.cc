2bf1e14f276c563bbfbe564c250fa0f7ff6b6fd3
nxp80031 <ayush.raj@nxp.com>
2021-08-23 10:03:44 +0530

    Fix tag frequent disconnect and reconnect issue.
    Addition of presence check timer at JNI causing two timer values
    for each tag one from libnfc and other from JNI. Since JNI timer
    is smaller compared to libnfc it always elapses first and leads to
    tag disconnect. To avoid this behavior JNI timer is removed to
    ensure only one timer for presence check handling.

--- SN100x/halimpl/hal/phNxpNciHal_IoctlOperations.cc
+++ SN100x/halimpl/hal/phNxpNciHal_IoctlOperations.cc
@@ -203,7 +203,6 @@ std::set<string> gNciConfigs = {"NXP_SE_COLD_TEMP_ERROR_DELAY",
                                 "NXP_MINIMAL_FW_VERSION",
                                 "NXP_P2P_DISC_NTF_TIMEOUT",
                                 "NXP_RESTART_RF_FOR_NFCEE_RECOVERY",
-                                "NXP_PRESENCE_CHECK_TIMEOUT",
                                 "NXP_NFCC_RECOVERY_SUPPORT",
                                 "NXP_AGC_DEBUG_ENABLE",
                                 "NXP_EXTENDED_FIELD_DETECT_MODE"};
