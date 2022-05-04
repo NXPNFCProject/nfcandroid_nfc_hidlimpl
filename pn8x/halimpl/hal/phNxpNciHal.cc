65f461e320e1c6f21ca48353d05f8629bdd5da96
nxf56319 <sai.shwethas@nxp.com>
2022-05-04 17:53:30 +0530

    Conf file macro name alignment with AOSP

--- pn8x/halimpl/hal/phNxpNciHal.cc
+++ pn8x/halimpl/hal/phNxpNciHal.cc
@@ -3446,7 +3446,7 @@ void phNxpNciHal_getVendorConfig(
   if (GetNxpNumValue(NAME_DEFAULT_ROUTE, &num, sizeof(num))) {
     config.defaultRoute = num;
   }
-  if (GetNxpByteArrayValue(NAME_DEVICE_HOST_WHITE_LIST, (char*)buffer.data(),
+  if (GetNxpByteArrayValue(NAME_DEVICE_HOST_ALLOW_LIST, (char*)buffer.data(),
                            buffer.size(), &retlen)) {
     config.hostWhitelist.resize(retlen);
     for (int i = 0; i < retlen; i++) config.hostWhitelist[i] = buffer[i];
