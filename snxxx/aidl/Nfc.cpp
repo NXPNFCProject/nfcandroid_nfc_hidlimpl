655f89d7b06ccc5995e69b4a5f98b7a5e169ddb1
nxf38293 <lokanathareddy.virayagarri@nxp.com>
2022-11-28 17:02:43 +0530

    Added AIDL support.
    Change-Id: I7c0bbb9993b0a00bff7e65efff669b030a5ba282

---
+++ snxxx/aidl/Nfc.cpp
@@ -0,0 +1,225 @@
+/******************************************************************************
+ *
+ *  Copyright 2022 NXP
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ ******************************************************************************/
+
+#include "Nfc.h"
+
+#include <android-base/logging.h>
+
+#include "phNxpNciHal_Adaptation.h"
+#include "phNxpConfig.h"
+#include "phNfcStatus.h"
+
+namespace aidl {
+namespace android {
+namespace hardware {
+namespace nfc {
+
+std::shared_ptr<INfcClientCallback> Nfc::mCallback = nullptr;
+AIBinder_DeathRecipient* clientDeathRecipient = nullptr;
+
+void OnDeath(void* cookie) {
+    if (Nfc::mCallback != nullptr && !AIBinder_isAlive(Nfc::mCallback->asBinder().get())) {
+        LOG(INFO) << __func__ << " Nfc service has died";
+        Nfc* nfc = static_cast<Nfc*>(cookie);
+        nfc->close(NfcCloseType::DISABLE);
+    }
+}
+
+::ndk::ScopedAStatus Nfc::open(const std::shared_ptr<INfcClientCallback>& clientCallback) {
+    LOG(INFO) << "Nfc::open";
+    if (clientCallback == nullptr) {
+        LOG(INFO) << "Nfc::open null callback";
+        return ndk::ScopedAStatus::fromServiceSpecificError(
+                static_cast<int32_t>(NfcStatus::FAILED));
+    }
+    Nfc::mCallback = clientCallback;
+
+    clientDeathRecipient = AIBinder_DeathRecipient_new(OnDeath);
+    auto linkRet = AIBinder_linkToDeath(clientCallback->asBinder().get(), clientDeathRecipient,
+                                        this /* cookie */);
+    if (linkRet != STATUS_OK) {
+        LOG(ERROR) << __func__ << ": linkToDeath failed: " << linkRet;
+        // Just ignore the error.
+    }
+
+    int ret = phNxpNciHal_open(eventCallback, dataCallback);
+    LOG(INFO) << "Nfc::open Exit";
+    return ret == NFCSTATUS_SUCCESS ? ndk::ScopedAStatus::ok()
+                    : ndk::ScopedAStatus::fromServiceSpecificError(
+                              static_cast<int32_t>(NfcStatus::FAILED));
+}
+
+::ndk::ScopedAStatus Nfc::close(NfcCloseType type) {
+    LOG(INFO) << "Nfc::close";
+    if (Nfc::mCallback == nullptr) {
+        LOG(ERROR) << __func__ << "mCallback null";
+        return ndk::ScopedAStatus::fromServiceSpecificError(
+                static_cast<int32_t>(NfcStatus::FAILED));
+    }
+    int ret = 0;
+    if (type == NfcCloseType::HOST_SWITCHED_OFF) {
+        ret = phNxpNciHal_close(true);
+    } else {
+        ret = phNxpNciHal_close(false);
+    }
+    Nfc::mCallback = nullptr;
+    AIBinder_DeathRecipient_delete(clientDeathRecipient);
+    clientDeathRecipient = nullptr;
+    return ret == NFCSTATUS_SUCCESS ? ndk::ScopedAStatus::ok()
+                    : ndk::ScopedAStatus::fromServiceSpecificError(
+                              static_cast<int32_t>(NfcStatus::FAILED));
+}
+
+::ndk::ScopedAStatus Nfc::coreInitialized() {
+    LOG(INFO) << "Nfc::coreInitialized";
+    if (Nfc::mCallback == nullptr) {
+        LOG(ERROR) << __func__ << "mCallback null";
+        return ndk::ScopedAStatus::fromServiceSpecificError(
+                static_cast<int32_t>(NfcStatus::FAILED));
+    }
+    int ret = phNxpNciHal_core_initialized();
+
+    return ret == NFCSTATUS_SUCCESS ? ndk::ScopedAStatus::ok()
+                    : ndk::ScopedAStatus::fromServiceSpecificError(
+                              static_cast<int32_t>(NfcStatus::FAILED));
+}
+
+::ndk::ScopedAStatus Nfc::factoryReset() {
+    LOG(INFO) << "Nfc::factoryReset";
+    phNxpNciHal_do_factory_reset();
+    return ndk::ScopedAStatus::ok();
+}
+
+::ndk::ScopedAStatus Nfc::getConfig(NfcConfig* _aidl_return) {
+  LOG(INFO) << "Nfc::getConfig";
+  NfcConfig config;
+  unsigned long num = 0;
+  std::array<uint8_t, 10> buffer;
+  buffer.fill(0);
+  long retlen = 0;
+  memset(&config, 0x00, sizeof(NfcConfig));
+
+  if (GetNxpNumValue(NAME_NFA_POLL_BAIL_OUT_MODE, &num, sizeof(num))) {
+    config.nfaPollBailOutMode = (bool)num;
+  }
+  if (GetNxpNumValue(NAME_ISO_DEP_MAX_TRANSCEIVE, &num, sizeof(num))) {
+    config.maxIsoDepTransceiveLength = (uint32_t)num;
+  }
+  if (GetNxpNumValue(NAME_DEFAULT_OFFHOST_ROUTE, &num, sizeof(num))) {
+    config.defaultOffHostRoute = (uint8_t)num;
+  }
+  if (GetNxpNumValue(NAME_DEFAULT_NFCF_ROUTE, &num, sizeof(num))) {
+    config.defaultOffHostRouteFelica = (uint8_t)num;
+  }
+  if (GetNxpNumValue(NAME_DEFAULT_SYS_CODE_ROUTE, &num, sizeof(num))) {
+    config.defaultSystemCodeRoute = (uint8_t)num;
+  }
+  if (GetNxpNumValue(NAME_DEFAULT_SYS_CODE_PWR_STATE, &num, sizeof(num))) {
+    config.defaultSystemCodePowerState = num;
+  }
+  if (GetNxpNumValue(NAME_DEFAULT_ROUTE, &num, sizeof(num))) {
+    config.defaultRoute = (uint8_t)num;
+  }
+  if (GetNxpByteArrayValue(NAME_DEVICE_HOST_ALLOW_LIST, (char*)buffer.data(),
+                           buffer.size(), &retlen)) {
+    config.hostAllowlist.resize(retlen);
+    for (long i = 0; i < retlen; i++) config.hostAllowlist[i] = buffer[i];
+  }
+  if (GetNxpNumValue(NAME_OFF_HOST_ESE_PIPE_ID, &num, sizeof(num))) {
+    config.offHostESEPipeId = (uint8_t)num;
+  }
+  if (GetNxpNumValue(NAME_OFF_HOST_SIM_PIPE_ID, &num, sizeof(num))) {
+    config.offHostSIMPipeId = (uint8_t)num;
+  }
+  if ((GetNxpByteArrayValue(NAME_NFA_PROPRIETARY_CFG, (char*)buffer.data(),
+                            buffer.size(), &retlen)) &&
+      (retlen == 9)) {
+    config.nfaProprietaryCfg.protocol18092Active = (uint8_t)buffer[0];
+    config.nfaProprietaryCfg.protocolBPrime = (uint8_t)buffer[1];
+    config.nfaProprietaryCfg.protocolDual = (uint8_t)buffer[2];
+    config.nfaProprietaryCfg.protocol15693 = (uint8_t)buffer[3];
+    config.nfaProprietaryCfg.protocolKovio = (uint8_t)buffer[4];
+    config.nfaProprietaryCfg.protocolMifare = (uint8_t)buffer[5];
+    config.nfaProprietaryCfg.discoveryPollKovio = (uint8_t)buffer[6];
+    config.nfaProprietaryCfg.discoveryPollBPrime = (uint8_t)buffer[7];
+    config.nfaProprietaryCfg.discoveryListenBPrime = (uint8_t)buffer[8];
+  } else {
+    memset(&config.nfaProprietaryCfg, 0xFF, sizeof(ProtocolDiscoveryConfig));
+  }
+  if ((GetNxpNumValue(NAME_PRESENCE_CHECK_ALGORITHM, &num, sizeof(num))) &&
+      (num <= 2)) {
+    config.presenceCheckAlgorithm = (PresenceCheckAlgorithm)num;
+  }
+
+  *_aidl_return = config;
+  return ndk::ScopedAStatus::ok();
+}
+
+::ndk::ScopedAStatus Nfc::powerCycle() {
+  LOG(INFO) << "powerCycle";
+  if (Nfc::mCallback == nullptr) {
+    LOG(ERROR) << __func__ << "mCallback null";
+    return ndk::ScopedAStatus::fromServiceSpecificError(
+        static_cast<int32_t>(NfcStatus::FAILED));
+  }
+  int ret = phNxpNciHal_power_cycle();
+  return ret == NFCSTATUS_SUCCESS
+             ? ndk::ScopedAStatus::ok()
+             : ndk::ScopedAStatus::fromServiceSpecificError(
+                   static_cast<int32_t>(NfcStatus::FAILED));
+}
+
+::ndk::ScopedAStatus Nfc::preDiscover() {
+  LOG(INFO) << "preDiscover";
+  if (Nfc::mCallback == nullptr) {
+    LOG(ERROR) << __func__ << "mCallback null";
+    return ndk::ScopedAStatus::fromServiceSpecificError(
+        static_cast<int32_t>(NfcStatus::FAILED));
+  }
+  int ret = phNxpNciHal_pre_discover();
+  return ret == NFCSTATUS_SUCCESS
+             ? ndk::ScopedAStatus::ok()
+             : ndk::ScopedAStatus::fromServiceSpecificError(
+                   static_cast<int32_t>(NfcStatus::FAILED));
+}
+
+::ndk::ScopedAStatus Nfc::write(const std::vector<uint8_t>& data, int32_t* _aidl_return) {
+    LOG(INFO) << "write";
+    if (Nfc::mCallback == nullptr) {
+        LOG(ERROR) << __func__ << "mCallback null";
+        return ndk::ScopedAStatus::fromServiceSpecificError(
+                static_cast<int32_t>(NfcStatus::FAILED));
+    }
+    *_aidl_return = phNxpNciHal_write(data.size(), &data[0]);
+    return ndk::ScopedAStatus::ok();
+}
+::ndk::ScopedAStatus Nfc::setEnableVerboseLogging(bool enable) {
+    LOG(INFO) << "setVerboseLogging";
+    phNxpNciHal_setVerboseLogging(enable);
+    return ndk::ScopedAStatus::ok();
+}
+
+::ndk::ScopedAStatus Nfc::isVerboseLoggingEnabled(bool* _aidl_return) {
+    *_aidl_return = phNxpNciHal_getVerboseLogging();
+    return ndk::ScopedAStatus::ok();
+}
+
+}  // namespace nfc
+}  // namespace hardware
+}  // namespace android
+}  // namespace aidl
---
+++ snxxx/extns/impl/nxpnfc/aidl/Nxp_Features.h
@@ -0,0 +1,235 @@
+/******************************************************************************
+ *
+ *  Copyright 2022 NXP
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ ******************************************************************************/
+
+#if (NXP_EXTNS == TRUE)
+#include <stdint.h>
+#else
+#include <unistd.h>
+#endif
+#include <string>
+#ifndef NXP_FEATURES_H
+#define NXP_FEATURES_H
+
+#define STRMAX_2 100
+#define FW_MOBILE_MAJOR_NUMBER_PN553 0x01
+#define FW_MOBILE_MAJOR_NUMBER_PN551 0x05
+#define FW_MOBILE_MAJOR_NUMBER_PN48AD 0x01
+#define FW_MOBILE_MAJOR_NUMBER_PN81A 0x02
+#define FW_MOBILE_MAJOR_NUMBER_PN557 0x01
+#define FW_MOBILE_MAJOR_NUMBER_SN100U 0x010
+#define FW_MOBILE_MAJOR_NUMBER_SN220U 0x01
+
+/*Including T4T NFCEE by incrementing 1*/
+#define NFA_EE_MAX_EE_SUPPORTED 5
+
+#define JCOP_VER_3_3 3
+#define JCOP_VER_4_0 4
+#ifndef FW_LIB_ROOT_DIR
+#if (defined(__arm64__) || defined(__aarch64__) || defined(_M_ARM64))
+#define FW_LIB_ROOT_DIR "/vendor/lib64/"
+#else
+#define FW_LIB_ROOT_DIR "/vendor/lib/"
+#endif
+#endif
+#ifndef FW_BIN_ROOT_DIR
+#define FW_BIN_ROOT_DIR "/vendor/firmware/"
+#endif
+#ifndef FW_LIB_EXTENSION
+#define FW_LIB_EXTENSION ".so"
+#endif
+#ifndef FW_BIN_EXTENSION
+#define FW_BIN_EXTENSION ".bin"
+#endif
+using namespace std;
+typedef enum {
+  NFCC_DWNLD_WITH_VEN_RESET,
+  NFCC_DWNLD_WITH_NCI_CMD
+} tNFCC_DnldType;
+
+typedef enum {
+  DEFAULT_CHIP_TYPE = 0x00,
+  pn547C2 = 0x01,
+  pn65T,
+  pn548C2,
+  pn66T,
+  pn551,
+  pn67T,
+  pn553,
+  pn80T,
+  pn557,
+  pn81T,
+  sn100u,
+  sn220u,
+  pn560
+} tNFC_chipType;
+
+typedef struct {
+  /*Flags common to all chip types*/
+  uint8_t _NFCC_I2C_READ_WRITE_IMPROVEMENT : 1;
+  uint8_t _NFCC_MIFARE_TIANJIN : 1;
+  uint8_t _NFCC_SPI_FW_DOWNLOAD_SYNC : 1;
+  uint8_t _NFCEE_REMOVED_NTF_RECOVERY : 1;
+  uint8_t _NFCC_FORCE_FW_DOWNLOAD : 1;
+  uint8_t _NFA_EE_MAX_EE_SUPPORTED : 3;
+  uint8_t _NFCC_DWNLD_MODE : 1;
+} tNfc_nfccFeatureList;
+
+typedef struct {
+  uint8_t nfcNxpEse : 1;
+  tNFC_chipType chipType;
+  std::string _FW_LIB_PATH;
+  std::string _FW_BIN_PATH;
+  uint16_t _PHDNLDNFC_USERDATA_EEPROM_OFFSET;
+  uint16_t _PHDNLDNFC_USERDATA_EEPROM_LEN;
+  uint8_t _FW_MOBILE_MAJOR_NUMBER;
+  tNfc_nfccFeatureList nfccFL;
+} tNfc_featureList;
+
+extern tNfc_featureList nfcFL;
+#define IS_CHIP_TYPE_GE(cType) (nfcFL.chipType >= cType)
+#define IS_CHIP_TYPE_EQ(cType) (nfcFL.chipType == cType)
+#define IS_CHIP_TYPE_LE(cType) (nfcFL.chipType <= cType)
+#define IS_CHIP_TYPE_L(cType) (nfcFL.chipType < cType)
+#define IS_CHIP_TYPE_NE(cType) (nfcFL.chipType != cType)
+
+#define CONFIGURE_FEATURELIST(chipType)                                        \
+  {                                                                            \
+    nfcFL.chipType = chipType;                                                 \
+    switch (chipType) {                                                        \
+    case pn81T:                                                                \
+      nfcFL.chipType = pn557;                                                  \
+      nfcFL.nfcNxpEse = true;                                                  \
+      CONFIGURE_FEATURELIST_NFCC_WITH_ESE(chipType)                            \
+      break;                                                                   \
+    case pn80T:                                                                \
+      nfcFL.chipType = pn553;                                                  \
+      nfcFL.nfcNxpEse = true;                                                  \
+      CONFIGURE_FEATURELIST_NFCC_WITH_ESE(chipType)                            \
+      break;                                                                   \
+    case pn67T:                                                                \
+      nfcFL.chipType = pn551;                                                  \
+      nfcFL.nfcNxpEse = true;                                                  \
+      CONFIGURE_FEATURELIST_NFCC_WITH_ESE(chipType)                            \
+      break;                                                                   \
+    case pn66T:                                                                \
+      nfcFL.chipType = pn548C2;                                                \
+      nfcFL.nfcNxpEse = true;                                                  \
+      CONFIGURE_FEATURELIST_NFCC_WITH_ESE(chipType)                            \
+      break;                                                                   \
+    case pn65T:                                                                \
+      nfcFL.chipType = pn547C2;                                                \
+      nfcFL.nfcNxpEse = true;                                                  \
+      CONFIGURE_FEATURELIST_NFCC_WITH_ESE(chipType)                            \
+      break;                                                                   \
+    case sn100u:                                                               \
+      nfcFL.chipType = sn100u;                                                 \
+      nfcFL.nfcNxpEse = true;                                                  \
+      CONFIGURE_FEATURELIST_NFCC_WITH_ESE(chipType)                            \
+      break;                                                                   \
+    case sn220u:                                                               \
+      nfcFL.chipType = sn220u;                                                 \
+      nfcFL.nfcNxpEse = true;                                                  \
+      CONFIGURE_FEATURELIST_NFCC_WITH_ESE(chipType)                            \
+      break;                                                                   \
+    default:                                                                   \
+      nfcFL.nfcNxpEse = false;                                                 \
+      CONFIGURE_FEATURELIST_NFCC(chipType)                                     \
+    }                                                                          \
+  }
+
+#define CONFIGURE_FEATURELIST_NFCC_WITH_ESE(chipType)                          \
+  {                                                                            \
+    switch (chipType) {                                                        \
+    case pn81T:                                                                \
+      CONFIGURE_FEATURELIST_NFCC(pn557)                                        \
+      nfcFL.nfccFL._NFCC_SPI_FW_DOWNLOAD_SYNC = true;                          \
+      nfcFL.nfccFL._NFA_EE_MAX_EE_SUPPORTED = 4;                               \
+      break;                                                                   \
+    case sn100u:                                                               \
+      CONFIGURE_FEATURELIST_NFCC(sn100u)                                       \
+      nfcFL.nfccFL._NFCC_SPI_FW_DOWNLOAD_SYNC = true;                          \
+      nfcFL.nfccFL._NFA_EE_MAX_EE_SUPPORTED = 5;                               \
+      break;                                                                   \
+    case sn220u:                                                               \
+      CONFIGURE_FEATURELIST_NFCC(sn220u)                                       \
+      nfcFL.nfccFL._NFCC_SPI_FW_DOWNLOAD_SYNC = true;                          \
+      nfcFL.nfccFL._NFA_EE_MAX_EE_SUPPORTED = 4;                               \
+      break;                                                                   \
+    default:                                                                   \
+      break;                                                                   \
+    }                                                                          \
+  }
+
+#define CONFIGURE_FEATURELIST_NFCC(chipType)                           \
+  {                                                                    \
+    nfcFL._PHDNLDNFC_USERDATA_EEPROM_OFFSET = 0x023CU;                 \
+    nfcFL._PHDNLDNFC_USERDATA_EEPROM_LEN = 0x0C80U;                    \
+    nfcFL._FW_MOBILE_MAJOR_NUMBER = FW_MOBILE_MAJOR_NUMBER_PN48AD;     \
+    nfcFL.nfccFL._NFCC_DWNLD_MODE = NFCC_DWNLD_WITH_VEN_RESET;         \
+    switch (chipType) {                                                \
+      case pn557:                                                      \
+        nfcFL.nfccFL._NFCC_I2C_READ_WRITE_IMPROVEMENT = true;          \
+        STRCPY_FW("libpn557_fw")                                       \
+        STRCPY_FW_BIN("pn557")                                         \
+        break;                                                         \
+      case sn100u:                                                     \
+        nfcFL.nfccFL._NFCC_DWNLD_MODE = NFCC_DWNLD_WITH_NCI_CMD;       \
+        nfcFL.nfccFL._NFCC_I2C_READ_WRITE_IMPROVEMENT = true;          \
+        nfcFL.nfccFL._NFCC_MIFARE_TIANJIN = false;                     \
+        nfcFL.nfccFL._NFCC_FORCE_FW_DOWNLOAD = true;                   \
+        nfcFL._FW_MOBILE_MAJOR_NUMBER = FW_MOBILE_MAJOR_NUMBER_SN100U; \
+        STRCPY_FW("libsn100u_fw")                                      \
+        STRCPY_FW_BIN("sn100u")                                        \
+        break;                                                         \
+      case sn220u:                                                     \
+        nfcFL.nfccFL._NFCC_DWNLD_MODE = NFCC_DWNLD_WITH_NCI_CMD;       \
+        nfcFL.nfccFL._NFCC_I2C_READ_WRITE_IMPROVEMENT = true;          \
+        nfcFL.nfccFL._NFCC_MIFARE_TIANJIN = false;                     \
+        nfcFL.nfccFL._NFCC_FORCE_FW_DOWNLOAD = true;                   \
+        nfcFL._FW_MOBILE_MAJOR_NUMBER = FW_MOBILE_MAJOR_NUMBER_SN220U; \
+        STRCPY_FW("libsn220u_fw")                                      \
+        STRCPY_FW_BIN("sn220u")                                        \
+        break;                                                         \
+      case pn560:                                                      \
+        nfcFL.nfccFL._NFCC_DWNLD_MODE = NFCC_DWNLD_WITH_NCI_CMD;       \
+        nfcFL.nfccFL._NFCC_I2C_READ_WRITE_IMPROVEMENT = true;          \
+        nfcFL.nfccFL._NFCC_MIFARE_TIANJIN = false;                     \
+        nfcFL.nfccFL._NFCC_FORCE_FW_DOWNLOAD = true;                   \
+        nfcFL._FW_MOBILE_MAJOR_NUMBER = FW_MOBILE_MAJOR_NUMBER_SN220U; \
+        STRCPY_FW("libpn560_fw")                                       \
+        STRCPY_FW_BIN("pn560")                                         \
+        break;                                                         \
+      default:                                                         \
+        nfcFL.nfccFL._NFCC_FORCE_FW_DOWNLOAD = true;                   \
+        break;                                                         \
+    }                                                                  \
+  }
+
+#define STRCPY_FW_BIN(str) {                                                   \
+  nfcFL._FW_BIN_PATH.clear();                                                  \
+  nfcFL._FW_BIN_PATH.append(FW_BIN_ROOT_DIR);                                  \
+  nfcFL._FW_BIN_PATH.append(str);                                              \
+  nfcFL._FW_BIN_PATH.append(FW_BIN_EXTENSION);                                 \
+}
+#define STRCPY_FW(str1) {                                                      \
+  nfcFL._FW_LIB_PATH.clear();                                                  \
+  nfcFL._FW_LIB_PATH.append(FW_LIB_ROOT_DIR);                                  \
+  nfcFL._FW_LIB_PATH.append(str1);                                             \
+  nfcFL._FW_LIB_PATH.append(FW_LIB_EXTENSION);                                 \
+}
+#endif
