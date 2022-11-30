d07534cd6eab74b8511ea503d5ef6298cdedcea1
nxf56319 <sai.shwethas@nxp.com>
2022-11-30 12:00:35 +0530

    Added MW Hal Version Print
    Function to print the MW Hal version is added to the aidl file.
    
    Change-Id: I7307a4e4f5d29d0b652c66db74f934f0dfcd0bfd

--- snxxx/halimpl/hal/phNxpNciHal_ext.h
+++ snxxx/halimpl/hal/phNxpNciHal_ext.h
@@ -29,6 +29,7 @@
 #define NXP_NFC_PARAM_ID_SWP2 0xD4
 #define NXP_NFC_PARAM_ID_SWPUICC3 0xDC
 
+void printNfcMwVersion();
 void phNxpNciHal_ext_init(void);
 NFCSTATUS phNxpNciHal_process_ext_rsp(uint8_t* p_ntf, uint16_t* p_len);
 NFCSTATUS phNxpNciHal_send_ext_cmd(uint16_t cmd_len, uint8_t* p_cmd);
