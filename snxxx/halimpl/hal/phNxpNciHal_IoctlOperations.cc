6feefc0612f5d20e9acbaf69a73a9e2f509e27b0
nxf31698 <suryaprakash.konduru@nxp.com>
2024-06-20 16:28:10 +0530

    Add provision to support multiple HCI UICC pipe IDs
    - To support multiple UICC HCI pipe IDs from config file based
    on system/OEM configurations instead of limiting.
    - AIDL version creation to support MEP feature
    
    Bug: 349089282
    Test: manual test
    
    Change-Id: Ib7df85eed27d2329debc17a7ad47c9a545275abb

--- snxxx/halimpl/hal/phNxpNciHal_IoctlOperations.cc
+++ snxxx/halimpl/hal/phNxpNciHal_IoctlOperations.cc
@@ -192,7 +192,6 @@ std::set<string> gNciConfigs = {"NXP_SE_COLD_TEMP_ERROR_DELAY",
                                 "NXP_DISCONNECT_TAG_IN_SCRN_OFF",
                                 "NXP_CE_PRIORITY_ENABLED",
                                 "NXP_RDR_REQ_GUARD_TIME",
-                                "OFF_HOST_SIM2_PIPE_ID",
                                 "NXP_ENABLE_DISABLE_LOGS",
                                 "NXP_RDR_DISABLE_ENABLE_LPCD",
                                 "NXP_SUPPORT_NON_STD_CARD",
@@ -207,9 +206,7 @@ std::set<string> gNciConfigs = {"NXP_SE_COLD_TEMP_ERROR_DELAY",
                                 "NXP_NFCC_RECOVERY_SUPPORT",
                                 "NXP_AGC_DEBUG_ENABLE",
                                 "NXP_EXTENDED_FIELD_DETECT_MODE",
-                                "NXP_SE_SMB_TERMINAL_TYPE",
-                                "OFF_HOST_ESIM_PIPE_ID",
-                                "OFF_HOST_ESIM2_PIPE_ID"};
+                                "NXP_SE_SMB_TERMINAL_TYPE"};
 
 /****************************************************************
  * Local Functions
