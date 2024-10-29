8666dbafb6c8edb6522d1e9dc86b3436c6dd7b62
nxf89829 <bhanusurendra.narra@nxp.com>
2024-10-29 18:19:00 +0530

    Fix the Copyright issues
    Change-Id: I03940253b6e9d2fe6d8e35c1689a637a14af06fe

--- snxxx/halimpl_v2/common/phNfcStatus.h
+++ snxxx/halimpl_v2/common/phNfcStatus.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2018, 2020-2021 NXP
+ * Copyright 2010-2018, 2020-2021,2024 NXP
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
