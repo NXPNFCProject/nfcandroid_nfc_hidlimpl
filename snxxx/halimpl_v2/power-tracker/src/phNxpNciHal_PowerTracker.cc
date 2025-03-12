5ebee3f1d7c14185f4b32f3ab460b7ba902f1988
nxf45198 <fahim.akhtar@nxp.com>
2025-03-12 11:57:29 +0530

    [artf1221731] : Copyright year updated

--- snxxx/halimpl_v2/power-tracker/src/phNxpNciHal_PowerTracker.cc
+++ snxxx/halimpl_v2/power-tracker/src/phNxpNciHal_PowerTracker.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022-2023 NXP
+ * Copyright 2022-2023,2025 NXP
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
