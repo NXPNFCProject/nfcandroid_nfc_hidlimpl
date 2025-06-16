4279cd8f7838a654649a284b41836d2ffdd29f77
nxf45198 <fahim.akhtar@nxp.com>
2025-06-16 19:42:59 +0530

    Update copyright & MW Version to 16.07.00

--- snxxx/halimpl_v2/dnld/phNxpNciHal_Dnld.cc
+++ snxxx/halimpl_v2/dnld/phNxpNciHal_Dnld.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2024 NXP
+ * Copyright 2012-2025 NXP
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
